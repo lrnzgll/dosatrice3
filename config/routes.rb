653de7792a88bfd7c5eecec7c9643b0335396f46
Lorenzo Galli <lrnzgll@gmail.com>
2018-06-13 18:51:59 +0100

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get "/products", to: 'products#index'
+  get "/company", to: 'companies#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
