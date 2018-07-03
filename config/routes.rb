ed9792eb897d262ca9a8317545038b3aa2a53359
Lorenzo Galli <lrnzgll@gmail.com>
2018-07-03 20:39:24 +0200

    i18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  get "/products", to: 'products#index'
-  get "/company", to: 'companies#show'
-  get "/where", to: 'pages#where'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	scope '(:locale)', locale: /it/ do
+		root to: 'pages#home'
+		get "/products", to: 'products#index'
+		get "/company", to: 'companies#show'
+		get "/where", to: 'pages#where'
+	end
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
