8a330d6e90c2ed1680f7bd858472ffdcf06f8512
Lorenzo Galli <lrnzgll@gmail.com>
2018-07-03 16:49:03 +0200

    mappa e tawk

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get "/products", to: 'products#index'
   get "/company", to: 'companies#show'
+  get "/where", to: 'pages#where'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
